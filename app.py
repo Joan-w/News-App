ae46b3bf3fa054cf3d259f7b13197ee5c676652e
joan-w <wamuyujoan4@gmail.com>
2020-04-27 00:21:49 +0300

    Removint the init of bbc and instead initialting index.html file

--- app.py
+++ app.py
@@ -6,33 +6,6 @@ app = Flask(__name__)
 # Init
 @app.route('/')
 def index():
-    # newsapi = NewsApiClient(api_key='3bf0db982cf4456b88151b7d2546f619')
-    # topheadlines = newsapi.get_top_headlines(sources="bbc-news")
-
-    # articles = topheadlines['articles']
-
-    # pubAt = []
-    # author = []
-    # news = []
-    # desc = []
-    # url = []
-
-    # for i in range(len(articles)):
-    #     myarticles = articles[i]
-
-    #     pubAt.append(myarticles['publishedAt'])
-    #     author.append(myarticles['author'])
-    #     news.append(myarticles['title'])
-    #     desc.append(myarticles['description'])
-    #     url.append(myarticles['url'])
-
-    # mylist = zip(pubAt, author, news, desc, url)
-    
-
-    return render_template('index.html', context = mylist)
-
-@app.route('/bbc')
-def index():
     newsapi = NewsApiClient(api_key='3bf0db982cf4456b88151b7d2546f619')
     topheadlines = newsapi.get_top_headlines(sources="bbc-news")
 
@@ -56,61 +29,8 @@ def index():
     mylist = zip(pubAt, author, news, desc, url)
     
 
-    return render_template('bbc.html', context = mylist)
+    return render_template('index.html', context = mylist)
 
-@app.route('/fox')
-def index():
-    newsapi = NewsApiClient(api_key='3bf0db982cf4456b88151b7d2546f619')
-    topheadlines = newsapi.get_top_headlines(sources="fox-news")
-
-    articles = topheadlines['articles']
-
-    pubAt = []
-    author = []
-    news = []
-    desc = []
-    url = []
-
-    for i in range(len(articles)):
-        myarticles = articles[i]
-
-        pubAt.append(myarticles['publishedAt'])
-        author.append(myarticles['author'])
-        news.append(myarticles['title'])
-        desc.append(myarticles['description'])
-        url.append(myarticles['url'])
-
-    mylist = zip(pubAt, author, news, name, desc, url)
-    
-
-    return render_template('fox.html', context = mylist)
-
-@app.route('/cnn')
-def index():
-    newsapi = NewsApiClient(api_key='3bf0db982cf4456b88151b7d2546f619')
-    topheadlines = newsapi.get_top_headlines(sources="cnn-news")
-
-    articles = topheadlines['articles']
-
-    pubAt = []
-    author = []
-    news = []
-    desc = []
-    url = []
-
-    for i in range(len(articles)):
-        myarticles = articles[i]
-
-        pubAt.append(myarticles['publishedAt'])
-        author.append(myarticles['author'])
-        news.append(myarticles['title'])
-        desc.append(myarticles['description'])
-        url.append(myarticles['url'])
-
-    mylist = zip(pubAt, author, news, name, desc, url)
-    
-
-    return render_template('cnn.html', context = mylist)
 
 @app.route('/al-jazeera')
 def index():
