41ab5973cdb84fafa3d0c8ae319dd86259b87bfd
joan-w <wamuyujoan4@gmail.com>
2020-04-26 22:25:00 +0300

    Initializing a cnn news page

--- app.py
+++ app.py
@@ -85,6 +85,33 @@ def index():
 
     return render_template('index.html', context = mylist)
 
+@app.route('/')
+def index():
+    newsapi = NewsApiClient(api_key='3bf0db982cf4456b88151b7d2546f619')
+    topheadlines = newsapi.get_top_headlines(sources="cnn-news")
+
+    articles = topheadlines['articles']
+
+    pubAt = []
+    author = []
+    news = []
+    desc = []
+    url = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        pubAt.append(myarticles['publishedAt'])
+        author.append(myarticles['author'])
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        url.append(myarticles['url'])
+
+    mylist = zip(pubAt, author, news, name, desc, url)
+    
+
+    return render_template('index.html', context = mylist)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
