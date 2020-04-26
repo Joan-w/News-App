793cbb2a5dc7d1e2ba63a094a32d842a502bf77e
joan-w <wamuyujoan4@gmail.com>
2020-04-26 23:46:01 +0300

    Adding an author instance

--- app.py
+++ app.py
@@ -26,91 +26,91 @@ def index():
         desc.append(myarticles['description'])
         url.append(myarticles['url'])
 
-    mylist = zip(pubAt, author, news, name, desc, url)
+    mylist = zip(pubAt, author, news, desc, url)
     
 
     return render_template('index.html', context = mylist)
 
-@app.route('/al-jazeera')
-def al-jazeera():
-    newsapi = NewsApiClient(api_key='3bf0db982cf4456b88151b7d2546f619')
-    topheadlines = newsapi.get_top_headlines(sources="al-jazeera-news")
+# @app.route('/al-jazeera')
+# def index():
+#     newsapi = NewsApiClient(api_key='3bf0db982cf4456b88151b7d2546f619')
+#     topheadlines = newsapi.get_top_headlines(sources="al-jazeera-news")
 
-    articles = topheadlines['articles']
+#     articles = topheadlines['articles']
 
-    pubAt = []
-    author = []
-    news = []
-    desc = []
-    url = []
+#     pubAt = []
+#     author = []
+#     news = []
+#     desc = []
+#     url = []
 
-    for i in range(len(articles)):
-        myarticles = articles[i]
+#     for i in range(len(articles)):
+#         myarticles = articles[i]
 
-        pubAt.append(myarticles['publishedAt'])
-        author.append(myarticles['author'])
-        news.append(myarticles['title'])
-        desc.append(myarticles['description'])
-        url.append(myarticles['url'])
+#         pubAt.append(myarticles['publishedAt'])
+#         author.append(myarticles['author'])
+#         news.append(myarticles['title'])
+#         desc.append(myarticles['description'])
+#         url.append(myarticles['url'])
 
-    mylist = zip(pubAt, author, news, name, desc, url)
+#     mylist = zip(pubAt, author, news, name, desc, url)
     
 
-    return render_template('index.html', context = mylist)
+#     return render_template('index.html', context = mylist)
 
-@app.route('/')
-def index():
-    newsapi = NewsApiClient(api_key='3bf0db982cf4456b88151b7d2546f619')
-    topheadlines = newsapi.get_top_headlines(sources="fox-news")
+# @app.route('/')
+# def index():
+#     newsapi = NewsApiClient(api_key='3bf0db982cf4456b88151b7d2546f619')
+#     topheadlines = newsapi.get_top_headlines(sources="fox-news")
 
-    articles = topheadlines['articles']
+#     articles = topheadlines['articles']
 
-    pubAt = []
-    author = []
-    news = []
-    desc = []
-    url = []
+#     pubAt = []
+#     author = []
+#     news = []
+#     desc = []
+#     url = []
 
-    for i in range(len(articles)):
-        myarticles = articles[i]
+#     for i in range(len(articles)):
+#         myarticles = articles[i]
 
-        pubAt.append(myarticles['publishedAt'])
-        author.append(myarticles['author'])
-        news.append(myarticles['title'])
-        desc.append(myarticles['description'])
-        url.append(myarticles['url'])
+#         pubAt.append(myarticles['publishedAt'])
+#         author.append(myarticles['author'])
+#         news.append(myarticles['title'])
+#         desc.append(myarticles['description'])
+#         url.append(myarticles['url'])
 
-    mylist = zip(pubAt, author, news, name, desc, url)
+#     mylist = zip(pubAt, author, news, name, desc, url)
     
 
-    return render_template('index.html', context = mylist)
+#     return render_template('index.html', context = mylist)
 
-@app.route('/')
-def index():
-    newsapi = NewsApiClient(api_key='3bf0db982cf4456b88151b7d2546f619')
-    topheadlines = newsapi.get_top_headlines(sources="cnn-news")
+# @app.route('/')
+# def index():
+#     newsapi = NewsApiClient(api_key='3bf0db982cf4456b88151b7d2546f619')
+#     topheadlines = newsapi.get_top_headlines(sources="cnn-news")
 
-    articles = topheadlines['articles']
+#     articles = topheadlines['articles']
 
-    pubAt = []
-    author = []
-    news = []
-    desc = []
-    url = []
+#     pubAt = []
+#     author = []
+#     news = []
+#     desc = []
+#     url = []
 
-    for i in range(len(articles)):
-        myarticles = articles[i]
+#     for i in range(len(articles)):
+#         myarticles = articles[i]
 
-        pubAt.append(myarticles['publishedAt'])
-        author.append(myarticles['author'])
-        news.append(myarticles['title'])
-        desc.append(myarticles['description'])
-        url.append(myarticles['url'])
+#         pubAt.append(myarticles['publishedAt'])
+#         author.append(myarticles['author'])
+#         news.append(myarticles['title'])
+#         desc.append(myarticles['description'])
+#         url.append(myarticles['url'])
 
-    mylist = zip(pubAt, author, news, name, desc, url)
+#     mylist = zip(pubAt, author, news, name, desc, url)
     
 
-    return render_template('index.html', context = mylist)
+#     return render_template('index.html', context = mylist)
 
 
 if __name__ == '__main__':
