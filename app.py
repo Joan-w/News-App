2040d0002c2e54446de0f899d3297cd2c29ac3ab
joan-w <wamuyujoan4@gmail.com>
2020-04-26 22:23:44 +0300

    Adding an al-jazeera news page

--- app.py
+++ app.py
@@ -31,6 +31,33 @@ def index():
 
     return render_template('index.html', context = mylist)
 
+@app.route('/al-jazeera')
+def al-jazeera():
+    newsapi = NewsApiClient(api_key='3bf0db982cf4456b88151b7d2546f619')
+    topheadlines = newsapi.get_top_headlines(sources="al-jazeera-news")
+
+    articles = topheadlines['articles']
+
+    pubAt = []
+    author = []
+    news = []
+    desc = []
+    url = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        pubAt.append(myarticles['publishedAt'])
+        author.append(myarticles['author'])
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        url.append(myarticles['url'])
+
+    mylist = zip(pubAt, author, news, name, desc, url)
+    
+
+    return render_template('index.html', context = mylist)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
